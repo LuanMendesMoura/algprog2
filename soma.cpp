f85544c8f88a711b84f2996633c26539ccce71c6
202619050097 <202619050097@lab01-l>
2026-08-24 09:53:02 -0400

    first commit

---
+++ soma.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int soma(int N)
+{
+	if(N == 1) {
+		return 1;
+	} else {
+		return soma(N-1) + N;
+	}
+
+}
+
+int main() 
+{
+	int N;
+	scanf("%d", &N);
+	printf("%d\n", soma(N));
+
+	return 0;
+}
