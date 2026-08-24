8f934b73499536afd4225228f82074f2d6880c17
Luan Mendes Moura <luanmendesmoura091207@gmail.com>
2026-08-24 10:08:46 -0400

    soma.cpp (ensinando repercussão)

--- soma.cpp
+++ soma.cpp
@@ -1,20 +1,42 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int soma(int N)
 {
-	if(N == 1) {
+	if(N == 1)
 		return 1;
-	} else {
+	else 
 		return soma(N-1) + N;
-	}
-
 }
 
-int main() 
+int main()
 {
 	int N;
+
 	scanf("%d", &N);
 	printf("%d\n", soma(N));
+
+	return 0;
+}
+
+/*****************************************/
+#include<stdio.h>
+
+int potencia(int x, int n)
+{
+	if(n == 0)
+		return 1;
+	else if(n == 1)
+		return x;
+	else 
+		return x * potencia(x, n-1);
+}
+
+int main()
+{
+	int x, n;
+
+	scanf("%d%d", &x, &n);
+	printf("%d\n", potencia(x, n));
 
 	return 0;
 }
